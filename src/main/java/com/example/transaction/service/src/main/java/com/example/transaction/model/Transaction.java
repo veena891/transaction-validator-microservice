7ccbd5eccde3f07d1b8ad44bcde444727bcdb312
Veena Gosangari <veenagosangari18@gmail.com>
2026-03-06 12:53:49 -0600

    Create Transaction.java

---
+++ src/main/java/com/example/transaction/service/src/main/java/com/example/transaction/model/Transaction.java
@@ -0,0 +1,10 @@
+package com.example.transaction.model;
+
+public class Transaction {
+
+    private String transactionId;
+    private double amount;
+    private String currency;
+    private String status;
+
+}
